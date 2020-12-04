a9ccb735f968d76a86f12131d4206d1bf701cc47
kiprox <70131116+kiprox@users.noreply.github.com>
2020-12-04 23:34:18 +0700

    Update util.cdn.js

--- goindex-acrou/src/libs/util.cdn.js
+++ goindex-acrou/src/libs/util.cdn.js
@@ -1,6 +1,6 @@
 export default function cdnpath(path) {
   let cdn = process.env.VUE_APP_CDN_PATH;
-  if (process.env.NODE_ENV === "release") {
+  if (process.env.NODE_ENV === "production") {
     return cdn + path;
   }
   return path;
